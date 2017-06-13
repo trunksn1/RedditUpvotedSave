787706e6e2e6388caae9f4576f0c98c2e9025695
trunksn1 <trunksn1@gmail.com>
2017-06-13 22:12:02 +0200

    aggiunti alcuni commenti e alcune funzionalità

--- RSU.py
+++ RSU.py
@@ -1,9 +1,12 @@
+#!/usr/bin/python 3.6
+
 import os, pprint, requests, shelve, sys
 import bs4, praw
 
 from gfycat.client import GfycatClient
 
 FILE_PRAW = 'praw.ini'
+PATH_SLUT = 'Y:\\Giochi\\Mega'
 PATH_SLUT_IMG = 'Y:\\Giochi\\Mega\\nsfw_img'
 PATH_SLUT_VID = 'Y:\\Giochi\\Mega\\nsfw_vid'
 
@@ -12,14 +15,14 @@ def main():
 	#se non esiste crealo
 	#altrimenti fai subito il login
 	
-	os.chdir(inizializza())
+	cartella_user = os.chdir(inizializza())
 	
 	reddit = praw.Reddit('rus', user_agent='RedditUpvotedSave (by /u/jacnk3)')
 	
 	sfigatto = GfycatClient()
 	
 	lista_imgup, lista_vidup = upvote_redditore(reddit, sfigatto)
-	
+
 	img(lista_imgup) 
 	vid(lista_vidup)
 	
@@ -37,6 +40,7 @@ def inizializza():
 		os.makedirs(cartella, exist_ok=True)
 		configFile = shelve.open(os.path.join(cartella, 'config'))
 		configFile['username'] = username
+		#TODO: va messo un check casomai l'utente sbaglia la password!
 		configFile['password'] = input ('what\'s /u/%s password?\n' %username)
 		os.chdir('y:')
 		os.makedirs(PATH_SLUT_IMG, exist_ok=True)
@@ -47,7 +51,6 @@ def inizializza():
 	crea_prawini(configFile)
 	return (cartella)
 
-	
 def crea_prawini(configFile):
 	with open (FILE_PRAW, 'w') as fileini:
 		'''Crea il file praw.ini da usare successivamente'''
@@ -58,40 +61,82 @@ def crea_prawini(configFile):
 		fileini.write('client_id=IIjAZV_ce3rkgA\n')
 		fileini.write('client_secret=qXdKaWzr9CxBEsFGto0IEgHtKEg')
 		
-
-#x = reddit.subreddit('redditdev')
-#print(x)
-
-#print(x.display_name)  # Output: redditdev
-#print(x.title)         # Output: reddit Development
-#print(x.description)   # Output: A subreddit for discussion of ...
-
-def upvote_redditore (reddit, sfigatto):
-	redditore = reddit.user.me()             
+def upvote_redditore (reddit, sfigatto):    
 	lista_immagini = list()
 	lista_video = list()
 	listone = list()
+	
+	redditore = reddit.user.me()         
+	printa_post(redditore)
+	
 	for upvote in redditore.upvoted():
+		url = upvote.url
+		lista_urls = upvote_passati(redditore)
+		
+		
 		#TODO: piuttosto che questo check sciocco
 		#creare un file con una lista di tutti i subreddit interessanti
 		if upvote.over_18 and upvote.subreddit != 'dwarffortress':
-			listone.append(upvote.url.rstrip('?1'))
-			if str(upvote.url).endswith('.jpg') or str(upvote.url).rstrip('?1').endswith('.png') or str(upvote.url).endswith('.gif'):
-				lista_immagini.append(upvote.url.rstrip('?1'))
-			elif str(upvote.url).startswith('https://gfycat.com/'):
-				sfnome = os.path.basename(upvote.url)
-				sfinfo = sfigatto.query_gfy(sfnome)
-				pprint.pprint (sfinfo)
-				sfurl = sfinfo['gfyItem']['mp4Url']
-				#video = 'https://giant.gfycat.com/' + str(upvote.url)[18:] + '.webm'
-				lista_video.append(sfurl)
-			else:
-				lista_immagini.append(upvote.url.rstrip('?1'))
-	pprint.pprint(listone)
+			listone.append(url.rstrip('?1'))
+			
+			#YODO: se l'url è in lista ed il file è nel computer salta il resto e togli l'upvote.
+			if doppione(url, lista_urls):
+				continue
+			else:			
+				if str(url).endswith('.jpg') or str(url).rstrip('?1').endswith('.png') or str(url).endswith('.gif'):
+					lista_immagini.append(url.rstrip('?1'))
+				elif str(url).startswith('https://gfycat.com/'):
+					sfnome = os.path.basename(url)
+					sfinfo = sfigatto.query_gfy(sfnome)
+					#pprint.pprint (sfinfo)
+					sfurl = sfinfo['gfyItem']['mp4Url']
+					#video = 'https://giant.gfycat.com/' + str(upvote.url)[18:] + '.webm'
+					lista_video.append(sfurl)
+				else:
+					lista_immagini.append(url.rstrip('?1'))
+		#pprint.pprint(listone)
 	return (lista_immagini, lista_video)
-#client_id: 2_C4C9qX
-#client_secret: cVT6HYz_9SxNirYk49kNHJcfTqdiy6qDlNtvqO-TOwR7-WKtrNTxNoyC2U-juJJ5
 
+def printa_post(utente):
+	num = 1
+	for up in utente.upvoted():
+		sub = str(up.subreddit)
+		if len(sub) > 12:
+			spazi = '\t'
+		elif len(sub) > 6:
+			spazi = '\t\t'
+		else:
+			spazi = '\t\t\t'
+		print (str(num) + ') /r/',up.subreddit, spazi, up.title.encode(errors='replace'))
+		num += 1
+	
+def upvote_passati(utente):
+	#TODO: cerca la lista dei doppioni in PATH_SLUT. Se c'è l'apre e si prepara ad
+	#controllare se i post dell'utente già ci sono e nel caso li salto (e toglie l'upvote)
+	#aggiunge i post mancanti
+	os.chdir(PATH_SLUT)
+	if not os.path.isfile(os.path.join(PATH_SLUT, 'listone.txt')):
+		modo = 'w'
+	else:
+		modo = 'a'
+	#with open ('listone.txt', modo) as lista:
+	#	return lista
+	lista = open ('lista_upvote.txt', modo)
+	lista.write('\n')
+	return lista
+	
+def doppione(url, lista_url):
+	lettura = lista_url.readlines()
+	if url not in lettura:
+		
+		lista_url.write(url)
+		return False
+	elif os.path.isfile(os.path.join(PATH_SLUT_IMG, os.path.basename(url))) or os.path.isfile(os.path.join(PATH_SLUT_VID, os.path.basename(url))):
+		print('Url già presente in lista')
+		return True
+		#TODO: controllare se il file esiste ancora nella cartella...
+	
+	
 def img(lista):
 	for i in range(len(lista)):
 		res = requests.get(lista[i])
@@ -102,6 +147,30 @@ def img(lista):
 		else:		
 			imageFile = open(os.path.join(PATH_SLUT_IMG, os.path.basename(lista[i])) , 'wb')
 		salva(imageFile, res)
+
+def vid(lista):	
+	for i in range(len(lista)):
+		res = requests.get(lista[i])
+		res.raise_for_status()	
+		vidFile = open(os.path.join(PATH_SLUT_VID, os.path.basename(lista[i])) , 'wb')
+		salva (vidFile, res)
+	
+def salva(path, res):
+	for pezzo in res.iter_content(100000):
+		path.write(pezzo)
+	path.close()
+	
+if __name__ == "__main__":
+	main()
+	ciao
+
+#x = reddit.subreddit('redditdev')
+#print(x)
+
+#print(x.display_name)  # Output: redditdev
+#print(x.title)         # Output: reddit Development
+#print(x.description)   # Output: A subreddit for discussion of ...
+
 	#for i in range(len(lista)):
 	#	res = requests.get(lista[i])
 	#	res.raise_for_status()
@@ -109,20 +178,8 @@ def img(lista):
 	#	for pezzo in res.iter_content(100000):
 	#		imageFile.write(pezzo)
 	#	imageFile.close()
-		
-def vid(lista):	
-	for i in range(len(lista)):
-		res = requests.get(lista[i])
-		res.raise_for_status()	
-		vidFile = open(os.path.join(PATH_SLUT_VID, os.path.basename(lista[i])) , 'wb')
-		salva (vidFile, res)
 
-	
-def salva(path, res):
-	for pezzo in res.iter_content(100000):
-		path.write(pezzo)
-	path.close()
-	
+
 #	soup = bs4.BeautifulSoup(res.text , "html.parser")
 #	imag = soup.select('img src')
 	#try:
@@ -138,5 +195,3 @@ def salva(path, res):
 #		print(upvote.thumbnail)
 #		print(str(upvote.thumbnail_height) + '*' + str(upvote.thumbnail_width))
 
-if __name__ == "__main__":
-	main()
